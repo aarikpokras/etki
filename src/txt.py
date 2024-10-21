69a8bc08118ca7276a450d1f579189cf82ed85eb
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-10-21 09:08:57 -0400

    Update txt.py to work with test

--- src/txt.py
+++ src/txt.py
@@ -2,4 +2,9 @@ import matplotlib.pyplot as plt
 
 class Text:
   def __init__(self, content, x, y, color='black', size=12):
+    self.x = x
+    self.y = y
+    self.content = content
+    self.color = color
+    self.size = size
     plt.text(x, y, content, color=color, fontsize=size)
