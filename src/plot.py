2b11073019de705e4603f50404762f5e3c3dd9cc
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-10-20 23:16:45 -0400

    Add self variables

--- src/plot.py
+++ src/plot.py
@@ -4,6 +4,11 @@ plt.rcParams['toolbar'] = 'None'
 
 class Line:
   def __init__(self, x1, y1, x2, y2, color='black'):
+    self.x1 = x1
+    self.y1 = y1
+    self.x2 = x2
+    self.y2 = y2
+    self.color = color
     x = [x1, x2]
     y = [y1, y2]
     plt.gca().set_aspect(1)
